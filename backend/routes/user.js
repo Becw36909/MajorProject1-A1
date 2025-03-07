665059d11c907a1ace921d40632636136d6afc96
Bec W <s3903758@student.rmit.edu.au>
2025-03-07 21:50:02 +1000

    added json to user route response

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,7 +4,17 @@ const router = express.Router()
 
 // GET - get all users
 router.get('/', (req, res) => {
-    res.send("Listing all users from the user route...")
+    // res.send("Listing all users from the user route...")
+    const users = [
+        {
+            _id: 1, 
+            firstName: "Jon",
+            lastName: "Snow",
+            email: "jsnow@fakemail.com"
+        }
+    ]
+
+    res.json(users)
 })
 
 module.exports = router
