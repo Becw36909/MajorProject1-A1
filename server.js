c79b689ed2d4955f1cd88ed6f5ae7845d3d3d812
Bec W <s3903758@student.rmit.edu.au>
2025-03-07 15:05:14 +1000

    initial commite, npm set up

---
+++ server.js
@@ -0,0 +1,26 @@
+// dependencies
+require("dotenv").config()
+const bodyParser = require("body-parser")
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+const port = process.env.PORT || 3000
+
+// database connection
+
+// express app setup
+const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use('*', cors())
+
+// routes
+// homepage
+app.get('/', (req, res) => {
+    res.send("This is the homepage")
+})
+
+// run app (listen on port)
+app.listen(port, () => {
+    console.log("App is running on port ", port)
+})
